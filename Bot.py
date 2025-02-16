4b67585ced263b3a3eaed0fde8665df677629196
SprayGoDFTW <157207256+SprayGoDFTW@users.noreply.github.com>
2025-02-16 23:18:41 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,9 +6,9 @@ API_HASH = '83fe20a435dbf635b67fc0ee3019a419'
 BOT_TOKEN = '7619930306:AAFVZuAhDVM_8cXHsjpTUH_MnyLuZl-LE_8'
 app = Client("Jinwoo", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@app.on_message(filters.text)
-def reply(client, message):
-    message.reply_text("Hello, World!")
+@app.on_message()
+def handle_message(client, message):
+    message.reply_text(f"Hello {message.from_user.first_name}!")
 
 if __name__ == "__main__":
     app.run()
