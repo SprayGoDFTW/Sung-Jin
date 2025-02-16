1c580d36d7b0f56e29adc5a23a25450a70f74a3b
SprayGoDFTW <157207256+SprayGoDFTW@users.noreply.github.com>
2025-02-16 23:12:07 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 API_ID = 26312191
 API_HASH = '83fe20a435dbf635b67fc0ee3019a419'
 BOT_TOKEN = '7619930306:AAFVZuAhDVM_8cXHsjpTUH_MnyLuZl-LE_8'
-app = Client("test", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("Jinwoo", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.text)
 def reply(client, message):
